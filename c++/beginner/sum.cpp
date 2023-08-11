d969681e86ae443ba5e9331ee29ccef768b42ad4
thisisrealnoman <nomanontheroad@gmail.com>
2023-08-11 17:08:54 +0600

    summation from user

---
+++ c++/beginner/sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2, sum;
+
+    cout<< "Enter two number for summation: ";
+
+    cin>> num1 >> num2;
+
+    sum = num1 + num2;
+
+    cout<< "Summation is = "<< sum;
+
+    getch();
+}
