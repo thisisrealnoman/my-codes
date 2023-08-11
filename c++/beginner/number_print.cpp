793416fee99f762d33923ea5e2e89ab3877852b7
thisisrealnoman <nomanontheroad@gmail.com>
2023-08-11 16:40:21 +0600

    first commit

---
+++ c++/beginner/number_print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+
+    int num1 = 50, num2 = 100;
+    char ch = 'a';
+
+    cout<< "Number1 = " <<num1 <<endl << "Number2 = " << num2 <<endl;
+    cout<< "Character = "<< ch;
+
+    getch();
+}
---
+++ c++/beginner/string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    char name[35] = "Abdullah Al Noman";
+
+    cout<< "My name is "<< name;
+
+    getch();
+}
